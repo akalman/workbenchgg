66d91086bf49cfdf35b58425f5f205764acd5558
akalman <dkeka.zkfaks+github@gmail.com>
2026-09-04 17:33:55 -0700

    move policy to pipeline role

--- lib/client-pipeline-stack.ts
+++ lib/client-pipeline-stack.ts
@@ -7,7 +7,7 @@ import { EnvironmentInfo } from '../config/environments';
 import { CodeStarConnectionsSourceAction, S3DeployAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Artifact } from 'aws-cdk-lib/aws-codepipeline';
 import { ConnectionArn } from '../config/constants';
-import { Effect, IRole, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { AnyPrincipal, Effect, IRole, PolicyStatement, Role } from 'aws-cdk-lib/aws-iam';
 
 export interface ClientStackProps extends StackProps {
     clientName: string;
@@ -22,6 +22,21 @@ export class ClientPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props: ClientStackProps) {
         super(scope, id, props);
 
+        const role = new Role(this, `ClientPipelineRole-${props.clientName}-${props.pipelineEnv.name}`, {
+            roleName: `ClientPipelineRole-${props.clientName}-${props.pipelineEnv.name}`,
+            assumedBy: new AnyPrincipal(),
+        });
+        role.addToPolicy(new PolicyStatement({
+            actions: [
+                'codeconnections:UseConnection',
+                'codeconnections:GetConnection',
+                'codeconnections:GetConnectionToken',
+                'codeconnections:ListConnections',
+                'codeconnections:PassConnection',
+            ],
+            resources: [ ConnectionArn ],
+        }));
+
         const synthStep = new ShellStep(`ClientPipelineBuild-${props.clientName}-${props.pipelineEnv.name}`, {
             input: CodePipelineSource.connection(`${props.client.author}/${props.client.package}`, props.client.branch, {
                 actionName: `${props.clientName}-source`,
@@ -34,19 +49,7 @@ export class ClientPipelineStack extends Stack {
             pipelineName: `ClientPipelineStack-${props.clientName}-${props.pipelineEnv.name}`,
             crossAccountKeys: true,
             synth: synthStep,
+            role: role,
         });
-
-        pipeline.buildPipeline();
-        pipeline.pipeline.role.addToPrincipalPolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: [
-                'codeconnections:UseConnection',
-                'codeconnections:GetConnection',
-                'codeconnections:GetConnectionToken',
-                'codeconnections:ListConnections',
-                'codeconnections:PassConnection',
-            ],
-            resources: [ ConnectionArn ],
-        }));
     }
 }
