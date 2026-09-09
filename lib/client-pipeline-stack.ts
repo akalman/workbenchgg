f71f85160eaafd1494ebe0045acbff31f1a03b48
akalman <dkeka.zkfaks+github@gmail.com>
2026-09-09 16:44:24 -0700

    reenable bucket policy

--- lib/client-pipeline-stack.ts
+++ lib/client-pipeline-stack.ts
@@ -147,15 +147,15 @@ export class ClientPipelineStack extends Stack {
 
         pipeline.buildPipeline();
 
-        // props.cdkBucket.addToResourcePolicy(new PolicyStatement({
-        //     effect: Effect.ALLOW,
-        //     principals: [new ArnPrincipal(buildRole.roleArn)],
-        //     actions: [
-        //         "s3:GetBucket*",
-        //         "s3:GetObject*",
-        //         "s3:List*",
-        //     ],
-        //     resources: [props.cdkBucket.bucketArn, `${props.cdkBucket.bucketArn}/*`],
-        // }));
+        props.cdkBucket.addToResourcePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            principals: [new ArnPrincipal(buildRole.roleArn)],
+            actions: [
+                "s3:GetBucket*",
+                "s3:GetObject*",
+                "s3:List*",
+            ],
+            resources: [props.cdkBucket.bucketArn, `${props.cdkBucket.bucketArn}/*`],
+        }));
     }
 }
